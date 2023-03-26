8409119e533aaa2546da118c68c79ffb63fb9e68
James Elazegui <jameselazegui21@gmail.com>
2023-03-27 12:53:36 +1300

    listings schema and get requests

--- backend/index.js
+++ backend/index.js
@@ -12,45 +12,72 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// models
+const Listing = require("./models/listing");
 const Member = require("./models/member");
 
 // ============== HTTP requests here ===============
 
+// all  members
 app.get("/members", async (req, res) => {
-    try {
-        const result = await Member.find({});
-        res.json(result);
-    } catch (error) {
-        res.send(error);
-    }
+   try {
+      const result = await Member.find({});
+      res.json(result);
+   } catch (error) {
+      res.send(error);
+   }
 });
 
+// single member
 app.get("/members/:username", async (req, res) => {
-    try {
-        const result = await Member.find({ username: req.params.username });
-        if (result.length > 0) {
-            res.json(result);
-        } else {
-            res.send("no match found");
-        }
-    } catch (error) {
-        res.send(error);
-    }
+   try {
+      const result = await Member.find({ username: req.params.username });
+      if (result.length > 0) {
+         res.json(result);
+      } else {
+         res.send("no match found");
+      }
+   } catch (error) {
+      res.send(error);
+   }
+});
+
+// ALl listings
+app.get("/listings", async (req, res) => {
+   const result = await Listing.find({});
+
+   res.json(result);
+});
+
+// listing by ID
+app.get("/listings/:id", async (req, res) => {
+   try {
+      const result = await Listing.find({ _id: req.params.id });
+      res.json(result);
+   } catch (error) {
+      console.log(error);
+      res.send(error);
+   }
+});
+
+// listing by member
+app.get("/listings/:member/listings", async (req, res) => {
+   const result = await Listing.find({ member: req.params.member });
+
+   res.json(result);
 });
 
 // =================================================
 
 // connection
 main().catch((error) => {
-    console.log(error);
+   console.log(error);
 });
 
 async function main() {
-    await mongoose.connect(process.env.DB_STRING);
+   await mongoose.connect(process.env.DB_STRING);
 }
 
 // listen server
 const port = process.env.PORT;
-app.listen(port || 5000, () =>
-    console.log(`server listening on port: ${port}`)
-);
+app.listen(port || 5000, () => console.log(`server listening on port: ${port}`));
