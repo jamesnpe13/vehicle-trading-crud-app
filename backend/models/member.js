76df47dc96dbd93d95fc7e1b4066b9dcfec631de
elliesaxby <ellie.saxby1@gmail.com>
2023-03-29 11:37:56 +1300

    Rating

--- backend/models/member.js
+++ backend/models/member.js
@@ -1,9 +1,11 @@
+const { number } = require("joi");
 const mongoose = require("mongoose");
 
 const MemberSchema = new mongoose.Schema({
-    username: {type:String,unique: true,required:true},
-    password: {type:String,required:true},
-    display_name: {type:String,required:true}
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    display_name: { type: String, required: true },
+    ratings: [],
 });
 
 module.exports = mongoose.model("member", MemberSchema);
