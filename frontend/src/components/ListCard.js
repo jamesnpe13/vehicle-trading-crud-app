12cf6113c951b5cb37e6b29f3fb664c3f82fec4b
elliesaxby <ellie.saxby1@gmail.com>
2023-04-04 11:07:17 +1200

    Card data (unfinished)

--- frontend/src/components/ListCard.js
+++ frontend/src/components/ListCard.js
@@ -1,6 +1,13 @@
 import "./ListCard.scss";
+import locationimg from "../images/location.svg";
+import carimg from "../images/car.svg";
+import fuelimg from "../images/fuel.svg";
+import odometerimg from "../images/odometer.svg";
+import personimg from "../images/person.svg";
+import transmissionimg from "../images/transmission.svg";
+import { useState } from "react";
 
-const Card = () => {
+const Card = ({ itemData }) => {
     return (
         <div className="card">
             <div>
@@ -10,20 +17,43 @@ const Card = () => {
                 ></img>
             </div>
             <div className="card-info">
-                <h1 className="list-title">List title</h1>
+                <h1 className="list-title">{itemData.title}</h1>
                 <div className="specs">
                     <div className="main-specs">
                         <p className="price">$10,000</p>
-                        <p>seller name</p>
-                        <p>location</p>
+                        <div className="card-item">
+                            <img src={personimg}></img>
+                            <p>seller name</p>
+                        </div>
+                        <div className="card-item">
+                            <img src={locationimg}></img>
+                            <p>{itemData.location}</p>
+                        </div>
                     </div>
                     <hr />
                     <div className="detail-specs">
-                        <p>Body type</p>
-                        <p>Seats</p>
-                        <p>Odometer</p>
-                        <p>Fuel</p>
-                        <p>Transmission</p>
+                        <div className="card-item">
+                            <img src={carimg}></img>
+                            <p>
+                                {itemData.vehicle.make} {itemData.vehicle.model}
+                            </p>
+                        </div>
+                        <div className="card-item">
+                            <img src={personimg}></img>
+                            <p>{itemData.vehicle.year}</p>
+                        </div>
+                        <div className="card-item">
+                            <img src={odometerimg}></img>
+                            <p>{itemData.vehicle.mileage}</p>
+                        </div>
+                        <div className="card-item">
+                            <img src={fuelimg}></img>
+                            <p>{itemData.vehicle.color}</p>
+                        </div>
+                        <div className="card-item">
+                            <img src={transmissionimg}></img>
+                            <p>{itemData.vehicle.transmission}</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,23 @@
 import "./Home.scss";
-
-export default function Home (){
-    return <h1>Home</h1>
-
-};
+import Card from "../components/ListCard";
+import { useEffect, useState } from "react";
+export default function Home() {
+    const [listingsData, setListingsData] = useState([]);
+    async function fetchData() {
+        const response = await fetch("http://localhost:5000/listings");
+        const data = await response.json();
+        setListingsData(data);
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
+    useEffect(() => {
+        console.log(listingsData);
+    }, [listingsData]);
+    return (
+        listingsData &&
+        listingsData.map((item) => {
+            return <Card key={item._id} itemData={item} />;
+        })
+    );
+}
