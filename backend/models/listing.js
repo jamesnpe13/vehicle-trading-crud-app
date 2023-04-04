67fc6605bc69a59c880cf26fa72d12d38a767987
elliesaxby <ellie.saxby1@gmail.com>
2023-04-04 12:43:14 +1200

    Splide and data addition

--- backend/models/listing.js
+++ backend/models/listing.js
@@ -1,41 +1,43 @@
 const mongoose = require("mongoose");
 
 const listingSchema = new mongoose.Schema({
-   owner_id: {
-      type: mongoose.Types.ObjectId,
-      required: true,
-      ref: "Member",
-   },
+    owner_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: "Member",
+    },
 
-   title: { type: String, required: true },
-   description: { type: String, required: true },
-   post_date: { type: String, required: true },
-   vehicle: new mongoose.Schema({
-      make: { type: String, required: true },
-      model: { type: String, required: true },
-      year: { type: String, required: true },
-      mileage: { type: String, required: true },
-      transmission: { type: String, required: true },
-      body_type: String,
-      color: String,
-      registration: new mongoose.Schema({
-         valid: { type: Boolean, required: true },
-         registration_number: { type: String, required: true },
-         expiration: { type: String, required: true },
-      }),
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: true },
+    location: { type: String, required: true },
+    post_date: { type: String, required: true },
+    vehicle: new mongoose.Schema({
+        make: { type: String, required: true },
+        model: { type: String, required: true },
+        year: { type: String, required: true },
+        mileage: { type: String, required: true },
+        transmission: { type: String, required: true },
+        body_type: String,
+        color: String,
+        registration: new mongoose.Schema({
+            valid: { type: Boolean, required: true },
+            registration_number: { type: String, required: true },
+            expiration: { type: String, required: true },
+        }),
 
-      wof: new mongoose.Schema({
-         valid: Boolean,
-         expiration: { type: String, required: true },
-      }),
-   }),
-   comments: [
-      new mongoose.Schema({
-         owner_id: String,
-         body: String,
-         post_date: String,
-      }),
-   ],
+        wof: new mongoose.Schema({
+            valid: Boolean,
+            expiration: { type: String, required: true },
+        }),
+    }),
+    comments: [
+        new mongoose.Schema({
+            owner_id: String,
+            body: String,
+            post_date: String,
+        }),
+    ],
 });
 
 module.exports = mongoose.model("Listing", listingSchema);
--- frontend/src/components/ListCard.js
+++ frontend/src/components/ListCard.js
@@ -6,15 +6,32 @@ import odometerimg from "../images/odometer.svg";
 import personimg from "../images/person.svg";
 import transmissionimg from "../images/transmission.svg";
 import { useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const Card = ({ itemData }) => {
     return (
         <div className="card">
             <div>
-                <img
-                    src="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png"
-                    alt="placeholderimg"
-                ></img>
+                <Splide
+                    className="splide"
+                    options={{
+                        rewind: true,
+                    }}
+                >
+                    <SplideSlide>
+                        <img
+                            src="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png"
+                            alt="img1"
+                        ></img>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img
+                            src="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png"
+                            alt="img2"
+                        ></img>
+                    </SplideSlide>
+                </Splide>
             </div>
             <div className="card-info">
                 <h1 className="list-title">{itemData.title}</h1>
@@ -23,7 +40,7 @@ const Card = ({ itemData }) => {
                         <p className="price">$10,000</p>
                         <div className="card-item">
                             <img src={personimg}></img>
-                            <p>seller name</p>
+                            <p>{itemData.owner_id.display_name}</p>
                         </div>
                         <div className="card-item">
                             <img src={locationimg}></img>
