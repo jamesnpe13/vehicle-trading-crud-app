a6c904429d0a7d34e61465e8480de85b976e65fb
James Elazegui <jameselazegui21@gmail.com>
2023-04-04 21:06:47 +1200

    fixed navbar code and styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import "./Navbar.scss";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import logo from "../images/logo.png";
 import hamburger from "../images/hamburger.png";
@@ -12,6 +12,7 @@ const Navbar = () => {
 	const [currentPage, setCurrentPage] = useState("");
 	const [sidebar, setSidebar] = useState(false);
 	const [signedIn, setSignedIn] = useContext(SignedInContext);
+	const navigate = useNavigate();
 
 	const showSidebar = () => setSidebar(!sidebar);
 
@@ -40,7 +41,15 @@ const Navbar = () => {
 
 	return (
 		<div className="navbar">
-			<img className="logo" src={logo} alt="logo"></img>
+			<img
+				className="logo"
+				src={logo}
+				alt="logo"
+				onClick={() => {
+					navigate("/listings");
+				}}
+			/>
+
 			<div className="links-desktop">
 				<Link to={"/mylistings"}>
 					<p>My Listings</p>
@@ -55,66 +64,147 @@ const Navbar = () => {
 					<p className="last">Customer Support</p>
 				</Link>
 			</div>
-			{/* <p className="location-text disabled">{currentPage}</p> */}
-			<IconContext.Provider value={{ color: "#f3f5f9" }}>
-				<img onClick={showSidebar} className="hamburger disabled" src={hamburger} alt="hamburger"></img>
 
-				<nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-					<ul className="nav-menu-items" onClick={showSidebar}>
-						<li className="navbar-toggle">
-							<div className="menu-bars">
-								<AiIcons.AiOutlineClose />
-							</div>
-						</li>
+			<img onClick={showSidebar} className="hamburger disabled" src={hamburger}></img>
 
-						<li className={"nav-text-b"}>
-							<Link to={"/listings"}>Home</Link>
-						</li>
-						<div className="links-group-1">
-							<li className={"nav-text-b"}>
-								<Link to={"/mylistings"}>My Listings</Link>
-							</li>
-							<li className={"nav-text"}>
-								<Link to={"/listings"}>View all listings</Link>
-							</li>
-							<li className={"nav-text"}>
-								<Link to={"/listings/create"}>Create listing</Link>
-							</li>
-						</div>
+			<nav className={sidebar ? "nav-menu active" : "nav-menu"} onClick={showSidebar}>
+				<div className="close-button">
+					<AiIcons.AiOutlineClose />
+				</div>
 
-						<div className="links-group-2">
-							<li className={"nav-text-b"}>
-								<Link to={"/account"}>Account</Link>
-							</li>
-							<li className={"nav-text"}>
-								<Link to={"/accountsetting"}>Account settings</Link>
-							</li>
-							<li className={"nav-text"}>
-								<Link to={"/transaction"}>Transaction history</Link>
-							</li>
-							<li className={"nav-text"}>
-								<Link to={"/carddetails"}>Card details</Link>
-							</li>
-						</div>
-						<div className="links-group-3">
-							<li className={"nav-text-b"}>
-								<Link to={"/customer"}>Customer service</Link>
-							</li>
-						</div>
-						<div className="links-group-4">
-							<li className={"nav-text-b"}>
-								<Link to={"/"} onClick={handleSignOut}>
-									Sign out
-								</Link>
-							</li>
-						</div>
-						{/* ) */}
-						{/* })} */}
-					</ul>
-				</nav>
-			</IconContext.Provider>
+				<ul className="links-group">
+					<li className={"nav-text-b"}>
+						<Link to={"/listings"}>Home</Link>
+					</li>
+				</ul>
+
+				<ul className="links-group">
+					<li className={"nav-text-b"}>
+						<Link to={"/mylistings"}>My Listings</Link>
+					</li>
+					<li className={"nav-text"}>
+						<Link to={"/listings"}>View all listings</Link>
+					</li>
+					<li className={"nav-text"}>
+						<Link to={"/listings/create"}>Create listing</Link>
+					</li>
+				</ul>
+
+				<ul className="links-group">
+					<li className={"nav-text-b"}>
+						<Link to={"/account"}>Account</Link>
+					</li>
+					<li className={"nav-text"}>
+						<Link to={"/accountsetting"}>Account settings</Link>
+					</li>
+					<li className={"nav-text"}>
+						<Link to={"/transaction"}>Transaction history</Link>
+					</li>
+					<li className={"nav-text"}>
+						<Link to={"/carddetails"}>Card details</Link>
+					</li>
+				</ul>
+				<ul className="links-group">
+					<li className={"nav-text-b"}>
+						<Link to={"/customer"}>Customer service</Link>
+					</li>
+				</ul>
+				<ul className="links-group signout">
+					<li className={"nav-text-b"}>
+						<Link to={"/"} onClick={handleSignOut}>
+							Sign out
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 };
 
 export default Navbar;
+
+// return (
+// 	<div className="navbar">
+// 		<img
+// 			className="logo"
+// 			src={logo}
+// 			alt="logo"
+// 			onClick={() => {
+// 				navigate("/listings");
+// 			}}
+// 		/>
+// 		<div className="links-desktop">
+// 			<Link to={"/mylistings"}>
+// 				<p>My Listings</p>
+// 			</Link>
+// 			<Link to={"/account"}>
+// 				<p>Account</p>
+// 			</Link>
+// 			<Link to={"/"} onClick={handleSignOut}>
+// 				<p>Sign Out</p>
+// 			</Link>
+// 			<Link to={"/customer"}>
+// 				<p className="last">Customer Support</p>
+// 			</Link>
+// 		</div>
+
+// 		<IconContext.Provider value={{ color: "#f3f5f9" }}>
+// 			<img onClick={showSidebar} className="hamburger disabled" src={hamburger}></img>
+
+// 			<nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+// 				<ul className="nav-menu-items" onClick={showSidebar}>
+// 					<div className="close-button">
+// 						<AiIcons.AiOutlineClose />
+// 					</div>
+
+// 					<div className="links-group">
+// 						<li className={"nav-text-b"}>
+// 							<Link to={"/listings"}>Home</Link>
+// 						</li>
+// 					</div>
+
+// 					<div className="links-group">
+// 						<li className={"nav-text-b"}>
+// 							<Link to={"/mylistings"}>My Listings</Link>
+// 						</li>
+// 						<li className={"nav-text"}>
+// 							<Link to={"/listings"}>View all listings</Link>
+// 						</li>
+// 						<li className={"nav-text"}>
+// 							<Link to={"/listings/create"}>Create listing</Link>
+// 						</li>
+// 					</div>
+
+// 					<div className="links-group">
+// 						<li className={"nav-text-b"}>
+// 							<Link to={"/account"}>Account</Link>
+// 						</li>
+// 						<li className={"nav-text"}>
+// 							<Link to={"/accountsetting"}>Account settings</Link>
+// 						</li>
+// 						<li className={"nav-text"}>
+// 							<Link to={"/transaction"}>Transaction history</Link>
+// 						</li>
+// 						<li className={"nav-text"}>
+// 							<Link to={"/carddetails"}>Card details</Link>
+// 						</li>
+// 					</div>
+// 					<div className="links-group">
+// 						<li className={"nav-text-b"}>
+// 							<Link to={"/customer"}>Customer service</Link>
+// 						</li>
+// 					</div>
+// 					<div className="links-group">
+// 						<li className={"nav-text-b"}>
+// 							<Link to={"/"} onClick={handleSignOut}>
+// 								Sign out
+// 							</Link>
+// 						</li>
+// 					</div>
+// 					{/* ) */}
+// 					{/* })} */}
+// 				</ul>
+// 			</nav>
+// 		</IconContext.Provider>
+// 	</div>
+// );
