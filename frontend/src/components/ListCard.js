17285b32396c90ba5642715d04b6506e91167144
Hyobs1 <hyobin963@gmail.com>
2023-04-12 08:43:12 +1200

    delete items

--- frontend/src/components/ListCard.js
+++ frontend/src/components/ListCard.js
@@ -14,6 +14,7 @@ import placeholderImg from "../images/graphic1.svg";
 import Options from "./Options";
 
 const Card = ({ itemData }) => {
+	
 	const [listingOwned, setListingOwned] = useState(false);
 
 	useEffect(() => {
@@ -35,6 +36,7 @@ const Card = ({ itemData }) => {
 			}
 		}
 	}
+	
 
 	return (
 		<div className="card">
@@ -131,6 +133,7 @@ const Card = ({ itemData }) => {
 					</div>
 				</div>
 			</div> */}
+			
 		</div>
 	);
 };
--- frontend/src/components/Options.js
+++ frontend/src/components/Options.js
@@ -3,12 +3,31 @@ import { useNavigate } from "react-router-dom";
 import "./Options.scss";
 import axios from "axios";
 import optionsImg from "../images/options.svg";
+import Listcards from "../components/ListCard"
 
-export default function Options({ listingOwned, listingId }) {
+
+
+export default function Options({ listingOwned, listingId, index }) {
 	const navigate = useNavigate();
 	const [dropdownIsActive, setDropdownIsActive] = useState(false);
 	const toggleDropdown = () => setDropdownIsActive(!dropdownIsActive);
 	const ref = useRef(null);
+
+	// const removeList = () => {
+	// 	listingId.removeList(index);
+	// }
+
+	const removeList = id => {
+		toggleDropdown();
+		listingOwned(Listcards => Listcards.filter(itemData => listingId !== id));
+
+	}
+	
+
+	// const removeList = (index) => {
+	// 	setDropdownIsActive(dropdownIsActive.filter((_, _index) => _index != index));
+	// }
+
 
 	const optionTypes = {
 		owner: () => {
@@ -17,7 +36,7 @@ export default function Options({ listingOwned, listingId }) {
 					<p className="option" data-path={`/account/listings/${listingId}/edit`} onClick={handleOptionSelect}>
 						Edit
 					</p>
-					<p className="option">Delete</p>
+					<p className="option" onClick={()=>removeList()}>Delete</p>
 				</>
 			);
 		},
@@ -33,6 +52,8 @@ export default function Options({ listingOwned, listingId }) {
 	function handleOptionSelect(e) {
 		navigate(e.target.getAttribute("data-path"));
 	}
+
+	
 
 	useEffect(() => {
 		let handler = (e) => {
